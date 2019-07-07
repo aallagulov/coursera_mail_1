acd446094ec3079d63a950ff09ef93c352970ba6
Anvar Allagulov <payable@yandex.ru>
2019-07-07 23:24:57 +0300

    copy initial files (test,etc)

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	f, err := os.OpenFile(out, os.O_RDONLY)
+	// fmt.Fprintln(out, path)
+	// return nil
+}
